ba82aca4fd1f01cfac21f2023edff946bd399201
mjia@redhat.com <mjia@redhat.com>
2015-04-16 16:47:03 +1000

    add beaker lab controller and distros tasks

---
+++ beaker-setup/roles/beaker/files/add_new_lc.sh
@@ -0,0 +1,10 @@
+# https://bugzilla.redhat.com/show_bug.cgi?id=1211119
+# Once the above issue is resolved, we could get rid of this.
+#!/bin/bash
+
+tmpdir=$(mktemp -d)
+trap "popd; rm -rf $tmpdir; exit" INT TERM EXIT
+pushd $tmpdir
+curl -c tmpcookie -d user_name=$1 -d password=$2 -d login=1 http://localhost/bkr/login || exit 1
+curl -b tmpcookie -d fqdn=$(hostname -f) -d lusername=host/localhost.localdomain -d lpassword=password \
+            -d email=root@localhost.localdomain -d Save=1 http://localhost/bkr/labcontrollers/save || exit 1
