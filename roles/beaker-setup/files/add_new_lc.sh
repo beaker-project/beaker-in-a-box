41448ce2425f8f77d178ea3f428b26d2f8803f2c
mjia@redhat.com <mjia@redhat.com>
2015-04-29 20:58:15 +1000

    clean up task rpms after adding

--- roles/beaker-setup/files/add_new_lc.sh
+++ roles/beaker-setup/files/add_new_lc.sh
@@ -8,3 +8,6 @@ pushd $tmpdir
 curl -c tmpcookie -d user_name=$1 -d password=$2 -d login=1 http://localhost/bkr/login || exit 1
 curl -b tmpcookie -d fqdn=$(hostname -f) -d lusername=host/localhost.localdomain -d lpassword=password \
             -d email=root@localhost.localdomain -d Save=1 http://localhost/bkr/labcontrollers/save || exit 1
+popd
+rm -rf $tmpdir
+trap - INT TERM EXIT
